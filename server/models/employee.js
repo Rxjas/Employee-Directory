22e64ef5bb2accf6d79ebaf26ebd966764a3cf85
Christopher Rojas <christopherrojas35@yahoo.com>
2020-12-13 15:15:35 -0700

    created employee model using mongoose

---
+++ server/models/employee.js
@@ -0,0 +1,29 @@
+const mongoose = require ('mongoose');
+
+const EmployeeSchema = mongoose.Schema({
+//time to setup properties for our employees
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    employeeid: {
+        type: String,
+        required: true
+    }
+});
+
+const Employee = mongoose.model('Employee', EmployeeSchema);
+
+module.exports = Employee;
