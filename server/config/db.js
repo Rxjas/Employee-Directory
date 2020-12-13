929ca8bc4224fe9a814622fd36af2d3b357c936f
Christopher Rojas <christopherrojas35@yahoo.com>
2020-12-13 15:04:06 -0700

    setup DB connection

---
+++ server/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+//async function needed since we need to try to connect to our server.
+async function db() {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_db', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+      });
+      console.log('successfuly connected to db');
+    } catch (error) {
+      console.log('error connecting to db: ');
+      console.log(error);
+    }
+  }
+
+  module.exports = db;
