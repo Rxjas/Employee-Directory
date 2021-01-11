f8aa1a8379d1500edb8ab989a4bc86ea17216fec
rxjas <christopherrojas35@yahoo.com>
2021-01-10 23:00:26 -0700

    Working on filtering

--- src/components/directory.jsx
+++ src/components/directory.jsx
@@ -2,17 +2,30 @@ import React from 'react';
 import Table from 'react-bootstrap/Table';
 
 function Directory (props){
+
     let employees = props.employees;
     return(
         <div>
-                {employees.map(person => (
-                    <ul key={person.id}>
-                    <li>{person.name}</li>
-                    <li>{person.title}</li>
-                    <li>{person.email}</li>
-                    <li>{person.id}</li>
-                    </ul>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Title</th>
+                        <th>Email</th>
+                        <th>ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map(person => (
+                        <tr key= {person.id}>
+                            <td>{person.name}</td>
+                            <td>{person.title}</td>
+                            <td>{person.email}</td>
+                            <td>{person.id}</td>
+                        </tr>
                     ))}
+                </tbody>
+            </Table>
         </div>//end of whole div
     );
 }
