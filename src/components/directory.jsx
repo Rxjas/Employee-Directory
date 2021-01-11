aaf9813429cda769f288e93af8ed71c37330d9fe
rxjas <christopherrojas35@yahoo.com>
2021-01-10 23:49:23 -0700

    added a form for filtering

--- src/components/directory.jsx
+++ src/components/directory.jsx
@@ -6,17 +6,28 @@ function Directory (props){
     let employees = props.employees;
     return(
         <div>
+            <form >
+                <label>I want to sort by...</label>
+                <select name="keys" id="keys">
+                    <option value="dob.date">Date of Birth</option>
+                    <option value="name.first">FirstName</option>
+                    <option value="name.last">LastName</option>
+                </select>
+                <select>
+                    <option value="ascending">Ascending</option>
+                    <option value="descending">Descending</option>
+                </select>
+                    <button type="submit">Filter!</button>
+            </form>
             <Table>
                 <thead>
                     <tr>
-                    <th>Employee ID</th>
-
+                        <th>Employee ID</th>
                         <th>FirstName</th>
                         <th>LastName</th>
                         <th>Email</th>
                         <th>Phone #</th>
                         <th>Date of Birth</th>
-
                     </tr>
                 </thead>
                 <tbody>
