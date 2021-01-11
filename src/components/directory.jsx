69c7f7d9a7ff9860c6fd59229d9b04a88866a41a
rxjas <christopherrojas35@yahoo.com>
2021-01-10 23:32:42 -0700

    Able to show the user info in a table

--- src/components/directory.jsx
+++ src/components/directory.jsx
@@ -9,19 +9,25 @@ function Directory (props){
             <Table>
                 <thead>
                     <tr>
-                        <th>Name</th>
-                        <th>Title</th>
+                    <th>Employee ID</th>
+
+                        <th>FirstName</th>
+                        <th>LastName</th>
                         <th>Email</th>
-                        <th>ID</th>
+                        <th>Phone #</th>
+                        <th>Date of Birth</th>
+
                     </tr>
                 </thead>
                 <tbody>
                     {employees.map(per => (
                         <tr key= {per.id}>
-                            <td>{per.name}</td>
-                            <td>{per.title}</td>
-                            <td>{per.email}</td>
                             <td>{per.id}</td>
+                            <td>{per.name.first}</td>
+                            <td>{per.name.last}</td>
+                            <td>{per.email}</td>
+                            <td>{per.phone}</td>
+                            <td>{per.dob.month}, {per.dob.day}</td>
                         </tr>
                     ))}
                 </tbody>
